34021ffeddc0d595b068fe467798e3cab0cb672f
umerkayvyro <muhammad.umer.khan@vyro.ai>
2025-07-08 00:22:55 +0500

    maybe

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/new-portfolio/',
+  build: {
+    outDir: 'docs',   // output goes in /docs
+  },
 })
